c9225b7f6029b0a3e169fbbcfd703714f60a4f1f
github username <github email address>
2019-11-13 14:23:18 -0500

    thanks jack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+before_action :set_current_user
+before_action :authorized
+
+  private
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authorized
+    redirect_to login_path unless @current_user
+  end
 end
