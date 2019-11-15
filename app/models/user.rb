ea4c4fcefe36c0a42be676a3ca0ccff178e772d5
github username <github email address>
2019-11-15 09:25:14 -0500

    friday morning

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ has_many :reviews
 has_many :rsvps, dependent: :destroy
 has_many :party_times, through: :rsvps
 has_many :businesses, through: :reviews
-validates :email, presence: true, uniqueness: true
+validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 validates :full_name, presence: true
 has_secure_password
+
+def username
+    self.email.split("@")[0]
+end
 
 end
